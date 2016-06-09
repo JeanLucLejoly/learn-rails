02646ad2b1c92e527f9a17f02a1c0754d68ff86f
JeanLucLejoly <jl@lejoly.com>
2016-06-08 21:09:26 -0700

    Work on how to do logging

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,9 @@
 class VisitorsController < ApplicationController
 	def new
+		Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
+		Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+		##raise 'deliberate Raise of a Problem'
 		render 'visitors/new'
 	end
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -9,6 +9,8 @@ class Owner
 	end
 
 	def countdown
+		Rails.logger.debug 'DEBUG: Entering in tthe countdown method'
+
 		today = Date.today
 		birthday = Date.new(today.year, birthdate.month, birthdate.day)
 		if birthday > today
@@ -16,6 +18,8 @@ class Owner
 		else
 			countdown = (birthday.next_year - today).to_i
 		end
+		Rails.logger.debug 'DEBUG: found countdown ' +countdown.to_s
+
 	end
 end
 
