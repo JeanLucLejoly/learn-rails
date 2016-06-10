e3c4c09186ce7d7fdf7bd8d1c1ead999f18c010d
JeanLucLejoly <jl@lejoly.com>
2016-06-10 13:43:02 -0700

    Work on Views, using flash messaging

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,10 @@ class VisitorsController < ApplicationController
 		Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
 		Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+		flash.now[:notice] = 'Welcome!'
+		flash[:alert] = 'My birthday is soon'
+
+
 		##raise 'deliberate Raise of a Problem'
 		render 'visitors/new'
 	end
