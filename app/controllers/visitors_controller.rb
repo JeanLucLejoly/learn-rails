1a2979b295185955cf21e226e64de2c39884c60f
JeanLucLejoly <jl@lejoly.com>
2016-06-17 17:51:23 -0700

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,12 +1,8 @@
 class VisitorsController < ApplicationController
 	def new
-		Rails.logger.debug 'DEBUG: entering new method'
+		##Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
-		Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
-		flash.now[:notice] = 'Welcome!'
-		flash[:alert] = 'My birthday is soon'
-
-
+		##Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
 		##raise 'deliberate Raise of a Problem'
 		render 'visitors/new'
 	end
